dc36ca9c072b6396bc1fe45a5d06307528818533
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-07-13 14:34:41 -0700

    Create and output simple components

--- script.js
+++ script.js
@@ -12,9 +12,38 @@ let list2= "Is popular"
 
 // replace content with variables
 // note can manip var
-let ul = ReactDOM.render(
-    <ul>
-        <li>{list1.toUpperCase()}</li>
-        <li>{list2}</li>
-    </ul>
-, document.getElementById("root"))
+// let ul = ReactDOM.render(
+//     <ul>
+//         <li>{list1.toUpperCase()}</li>
+//         <li>{list2}</li>
+//     </ul>
+// , document.getElementById("root"))
+
+//output to screen
+ReactDOM.render(<App />, document.getElementById("root"))
+
+// components
+function Header(){
+    return (
+        <header>
+            <h1>Amanda's Kitchen</h1>
+        </header>
+    )
+}
+
+function Main(){
+    return(
+        <section><p>We serve the best food around.</p></section>
+    )
+
+}
+
+// What will output to screen
+function App(){
+    return(
+        <div>
+            <Header />
+            <Main />
+        </div>
+    )
+}
