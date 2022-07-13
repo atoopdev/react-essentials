540b6603cdde6a524ede1ba8f3445f84e09c1a78
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-07-13 12:35:17 -0700

    convert to jsx

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 // what to create, style, content
 let heading = React.createElement("h1", null, "Reasons to Learn React")
-let ul = React.createElement("ul", {style: {color: "blue"}}, 
-React.createElement("li", null, "Is cool"), 
-React.createElement("li", null, "Is popular")
-)
-ReactDOM.render(ul, document.getElementById("root"))
+// let ul = React.createElement("ul", {style: {color: "blue"}}, 
+// React.createElement("li", null, "Is cool"), 
+// React.createElement("li", null, "Is popular")
+// )
+
+// redo above in JSX
+let ul = ReactDOM.render(
+    <ul>
+        <li>Is cool</li>
+        <li>Is popular</li>
+    </ul>
+, document.getElementById("root"))
